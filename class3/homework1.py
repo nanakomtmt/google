355b0db5a1035553d6f6a2e5f2003be09006acf8
“nanakomtmt” <nnk.mtmt11@outlook.jp>
2022-05-24 02:04:14 +0900

    テストケース追加

--- class3/homework1.py
+++ class3/homework1.py
@@ -121,9 +121,14 @@ def test(line):
 # Add more tests to this function :)
 def run_test():
     print("==== Test started! ====")
+    test("1")
     test("1+2")
     test("1.0+2.1-3")
+    test("1.0+5*4")
     test("5*3*4")
+    test("2.1*3")
+    test("1.0+2.0*3/6")
+    test("4+5+6/6/6")
     print("==== Test finished! ====\n")
 
 
